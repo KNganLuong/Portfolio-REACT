05849f30ddfeb49529b279777b289851dfa023bd
KNganLuong <kngan.luong@gmail.com>
2024-08-31 22:39:28 +0200

    modale added

--- src/components/datas/resumeData.js
+++ src/components/datas/resumeData.js
@@ -56,33 +56,61 @@ let resumeData = {
     {
       name: 'Create a Travel Agency Homepage with HTML & CSS',
       description: 'Travel Agency Homepage',
-      imgurl: 'images/portfolio/project-1/project-1-Booki.png',
+      imgurl: [
+        'images/portfolio/project-1/project-1-Booki.png',
+        'images/portfolio/project-1/project-1-Booki-1.png',
+      ],
     },
     {
       name: 'Improve the Interface of a Mobile Site with CSS animations',
       description: 'Mobile site with CSS animations',
-      imgurl: 'images/portfolio/project-2/project-2-Oh-my-food.png',
+      imgurl: [
+        'images/portfolio/project-2/project-2-Oh-my-food.png',
+        'images/portfolio/project-2/project-2-Oh-my-food-1.png',
+        'images/portfolio/project-2/project-2-Oh-my-food-2.png',
+        'images/portfolio/project-2/project-2-Oh-my-food-3.png',
+      ],
     },
     {
       name: 'Create a Dynamic Website using JavaScript',
       description: 'A website with JavaScript',
-      imgurl: 'images/portfolio/project-4/project-4-ArchiWebos.png',
+      imgurl: [
+        'images/portfolio/project-4/project-4-ArchiWebos.png',
+        'images/portfolio/project-4/project-4-ArchiWebos-1.png',
+        'images/portfolio/project-4/project-4-ArchiWebos-2.png',
+        'images/portfolio/project-4/project-4-ArchiWebos-3.png',
+        'images/portfolio/project-4/project-4-ArchiWebos-4.png',
+      ],
     },
     {
       name: 'Build a Real Estate Rental Web App with React',
       description: 'First Steps with React',
-      imgurl: 'images/portfolio/project-5/project-5-Kasa-1.png',
+      imgurl: [
+        'images/portfolio/project-5/project-5-Kasa-1.png',
+        'images/portfolio/project-5/project-5-Kasa-2.png',
+        'images/portfolio/project-5/project-5-Kasa-3.png',
+        'images/portfolio/project-5/project-5-Kasa-4.png',
+      ],
     },
     {
       name: 'Debug the Website of an Events Agency',
       description: 'Debug and complete the development of a one-page site',
-      imgurl: 'images/portfolio/project-6/project-6-724-events.png',
+      imgurl: [
+        'images/portfolio/project-6/project-6-724-events.png',
+        'images/portfolio/project-6/project-6-724-events-1.png',
+        'images/portfolio/project-6/project-6-724-events-2.png',
+        'images/portfolio/project-6/project-6-724-events-3.png',
+      ],
     },
     {
       name: 'Implementing the front end of a banking app with React and Redux',
       description:
         'Using React and Redux to develop the front end of a banking application and Writing REST API calls',
-      imgurl: 'images/portfolio/project-7/project-7-Argent-Bank-1.png',
+      imgurl: [
+        'images/portfolio/project-7/project-7-Argent-Bank-1.png',
+        'images/portfolio/project-7/project-7-Argent-Bank-2.png',
+        'images/portfolio/project-7/project-7-Argent-Bank-3.png',
+      ],
     },
   ],
 }
